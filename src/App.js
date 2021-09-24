aaf5fbd104a263b2caa2e9246e0c34080b461ac3
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-24 16:51:46 +0600

    folder structure added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import Photos from './components/Photos/Photos';
+
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Photos></Photos>
+      
     </div>
   );
 }
 
+/* 
 function Photos() {
   const [photos, setPhotos] = useState([]);
 
@@ -25,8 +29,19 @@ function Photos() {
     <div>
       <h2>Traveling around the world Photos!!</h2>
       <h4>Photos Available: {photos.length}</h4>
+      {
+        photos.map(photo => <Photo title={photo.title} id={photo.id}></Photo>)
+      }
     </div>
   );
 }
 
+function Photo(props) {
+  return (
+    <div>
+      <h2>Title: {props.title}</h2>
+      <p>Id: {props.id}</p>
+    </div>
+  );
+} */
 export default App;
